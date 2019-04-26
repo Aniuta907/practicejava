b0e82e0c72039f711399358d435a72be02e1acdc
Aniuta907 <anuta907email@gmail.com>
2019-04-26 14:24:08 +0500

    Магия

--- src/DocumentExistsException.java
+++ src/DocumentExistsException.java
@@ -1,5 +1,6 @@
 public class DocumentExistsException extends Exception{
+    // 123
     public DocumentExistsException(String message){
-        super(message);
+            super(message);
     }
 }
